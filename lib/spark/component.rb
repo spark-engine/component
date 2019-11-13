4d5a15f90e459703310f664f8aab793949056aa9
Brandon Mathis <brandon@imathis.com>
2019-11-12 18:05:53 -0600

    v1.0.0-alpha

---
+++ lib/spark/component.rb
@@ -0,0 +1,8 @@
+require "spark/component/version"
+
+module Spark
+  module Component
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/spark/component/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Spark
+  module Component
+    module VERSION
+      "1.0.0-alpha".freeze
+    end
+  end
+end
